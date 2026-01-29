de9edf0575307c64dc34c71ec5e16380f690f5ea
Evastrings <elijahakandetea@gmail.com>
2026-01-29 02:05:01 -0600

    fixes: fixed import error affecting deployment in railway

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import io
 
-from services.color_extractor import extract_dominant_colors
-from services.color_data import rgb_to_hex
+from app.services.color_extractor import extract_dominant_colors
+from app.services.color_data import rgb_to_hex
 
 
 app = FastAPI()
@@ -56,45 +56,3 @@ async def analyze_image(file: UploadFile = File(...)):
     extracted_colors = [dict(zip(keys, f_colors)) for f_colors in zip(hex_list, rgb_list, percentages)]
     
     return {"d_colors": extracted_colors}
-
-# import json
-
-# # Data for 5 individuals
-# names = ["Alice", "Jon", "Jane", "Vic", "Nelson"]
-# ids = [101, 102, 103, 104, 105]
-# cities = ["New York", "London", "Paris", "Berlin", "Tokyo"]
-
-# # 1. Define the keys once
-# keys = ["name", "id", "city"]
-
-# # 2. Use zip in a list comprehension to create a list of dictionaries
-# # Each iteration of zip(names, ids, cities) creates a tuple like ("Alice", 101, "New York")
-# data_list = [dict(zip(keys, person_data)) for person_data in zip(names, ids, cities)]
-
-# # 3. Convert the entire list to a formatted JSON string
-# json_output = json.dumps(data_list, indent=4)
-# print(json_output)
-
-
-# from services.color_extractor import extract_dominant_colors
-# from services.color_data import rgb_to_hex
-# from services.visualize_palette import visualize_palette
-# from services.palette_generator import save_palette_to_file
-
-
-# def main():
-#     image_path = "./images/megumi.jpg"
-#     n_colors = 5
-    
-#     colors, percentages = extract_dominant_colors(image_path, n_colors)
-    
-#     hex_codes = [rgb_to_hex(color) for color in colors]
-   
-#     visualize_palette(colors, percentages, hex_codes, output_path="output/megumi_palete.png")
-    
-#     save_palette_to_file(hex_codes, "output/hexcodes/megumicode.txt")
-    
-#     print("Anime Palette extraction complete!")
-
-# if __name__ == "__main__":
-#     main()
